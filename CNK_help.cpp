135e2c5c403dcad84204391b99fa16ca2b42c3c0
davillbert <56187467+davillbert@users.noreply.github.com>
2019-11-18 18:50:33 +0300

    Update CNK_help.cpp

--- CNK_help.cpp
+++ CNK_help.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 using ULL = unsigned long long;
 //using ULL = unit_64;
+ULL elementus[68][68];
 
 
 ULL C(ULL n, ULL k)
 {
    // ULL** elementus = (ULL**)malloc(n * sizeof(ULL));
    // for(ULL i = 0; i < n; ++i) elementus[i] = (ULL*)malloc(k*sizeof(ULL));
-    ULL elementus[67][67];
+    
     if(elementus[n][k]) return elementus[n][k];
     if(n == k) elementus[n][k] = 1;
     if(k == 0) elementus[n][k] = 1;
